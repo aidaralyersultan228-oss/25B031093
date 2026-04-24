53ec8f79f8ecb7a0c24714671dcce23caf0baaaf
ersultan <e_aidarali@kbtu.kz>
2026-04-24 17:10:36 +0500

    Add Practice1 Python basics examples

---
+++ Practice1/getting_started.py
@@ -0,0 +1,15 @@
+# 1) SyntaxError
+# print("Hello"        # SyntaxError: invalid syntax (missing ')')
+
+# 2) IndentationError
+# if True:
+# print("Hi")          # IndentationError: expected an indented block
+
+# 3) NameError
+# print(x)             # NameError: name 'x' is not defined
+
+# 4) TypeError
+# print("5" + 5)       # TypeError: can only concatenate str (not "int") to str
+
+# 5) ZeroDivisionError
+# print(1 / 0)         # ZeroDivisionError: division by zero
